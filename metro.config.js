4b157c7ed26fc50ee99e3667780f8ed931f83310
Juan1mera <juan1mera2barrera@gmail.com>
2025-01-01 15:49:32 -0500

    fix: Configuracion para mostrar SVGS

--- metro.config.js
+++ metro.config.js
@@ -6,14 +6,16 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {
+const defaultConfig = getDefaultConfig(__dirname);
+
+const customConfig = {
   transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'), // Soporte para SVG
   },
   resolver: {
-    assetExts: getDefaultConfig(__dirname).resolver.assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...getDefaultConfig(__dirname).resolver.sourceExts, 'svg'],
+    assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'), // Elimina SVG de assets
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'], // Agrega SVG como fuente
   },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, customConfig);
