b126a845e406367e092bebdf845d832cb3114e14
Juan1mera <juan1mera2barrera@gmail.com>
2025-01-11 18:22:19 -0500

    add: pantalla de carga

---
+++ src/components/global/LoaderScreen.tsx
@@ -0,0 +1,17 @@
+import { View, Text, ActivityIndicator } from 'react-native'
+import React from 'react'
+import { useTheme } from '../../lib/context/ThemeContext';
+import { getDynamicColors } from '../../assets/styles/colors';
+
+const LoaderScreen = () => {
+    const { isDarkTheme } = useTheme();
+    const colors = getDynamicColors(isDarkTheme);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color={colors.verde} />
+    </View>
+  )
+}
+
+export default LoaderScreen
