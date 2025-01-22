da71b4ceaf76991182d9ff821d54ee5fb50413aa
Juan1mera <juan1mera2barrera@gmail.com>
2025-01-22 14:13:23 -0500

    style: Cambio de tamaño en el header

--- src/components/Navigations/HeaderDrawer.tsx
+++ src/components/Navigations/HeaderDrawer.tsx
@@ -28,7 +28,7 @@ const createStyles = (colors: ReturnType<typeof getDynamicColors>) =>
   StyleSheet.create({
     container: {
       width: '100%',
-      height: 50,
+      height: 60,
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'center',
