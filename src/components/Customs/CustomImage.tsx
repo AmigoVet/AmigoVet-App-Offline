7c845384911bcdc354e7e280ec4b8a8f927df2f3
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-09 15:03:58 -0500

    fix: Acepta imagenes vacias

--- src/components/Customs/CustomImage.tsx
+++ src/components/Customs/CustomImage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { constants } from '../../assets/styles/constants';
 
 interface CustomImageProps {
-    source: string;
+    source: string | undefined;
     full?: boolean;
     style?: StyleProp<ImageStyle>;
 }
