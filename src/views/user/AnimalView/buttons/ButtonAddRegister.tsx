990ea1061a0fe7c1fc541ebedfdb6451f9f664a2
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-11 08:07:08 -0500

    feat: Modal para agregar un registro

--- src/views/user/AnimalView/buttons/ButtonAddRegister.tsx
+++ src/views/user/AnimalView/buttons/ButtonAddRegister.tsx
@@ -1,42 +1,125 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native'
-import React from 'react'
-import { useTheme } from '../../../../lib/context/ThemeContext';
-import { getDynamicColors, newColors } from '../../../../assets/styles/colors';
-import { CustomIcon } from '../../../../components/Customs';
+import React, { useRef, useState } from 'react'
+import { newColors } from '../../../../assets/styles/colors';
+import { CustomButton, CustomIcon, CustomInput, CustomSelect } from '../../../../components/Customs';
 import { Animal } from '../../../../lib/interfaces/animal';
+import { Modalize } from 'react-native-modalize';
+import { createGlobalStyles } from '../../../../assets/styles/styles';
+import { setDataRegister } from '../../../../lib/db/registers/setDataRegister';
 
 interface ButtonAddRegisterProps {
+  animalId: string;
     animal: Animal;
     onPress: () => void;
 }
 
-const ButtonAddRegister = ({onPress, animal}: ButtonAddRegisterProps) => {
-
+const ButtonAddRegister = ({ animalId, onPress, animal }: ButtonAddRegisterProps) => {
+    console.log(animalId)
     const styles = dynamicStyles();
+    const globalStyles = createGlobalStyles()
+    const modalRef = useRef<Modalize>(null);
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedValue, setSelectedValue] = useState<string>("");
+    const [comentario, setComentario] = useState<string>("");
+
+    const openModal = () => {
+        setIsOpen(true);
+        modalRef.current?.open();
+    };
+
+    const closeModal = () => {
+        setIsOpen(false);
+        modalRef.current?.close();
+    };
+
+    // Definir opciones con un valor interno y una etiqueta visible
+    const options = [
+        { label: "Registrar tratamiento", value: "tratamiento" },
+    ];
+
+    if (animal.genero === "Hembra") {
+        options.push(
+            { label: "Registrar preñez", value: "preñez" },
+            { label: "Registrar inseminación", value: "inseminacion" }
+        );
+        if (animal.embarazada) {
+            options.push({ label: "Registrar aborto", value: "aborto" });
+        }
+    }
+
+    const handleSave = async () => {
+        console.log({
+            id: Math.random().toString(36).substr(2, 9),
+            animalId: animalId,
+            comentario: comentario,
+            accion: 'Registro ' + selectedValue,
+            fecha: new Date().toISOString()
+        });
+        closeModal();
+    }
 
     return (
-        <Pressable style={styles.button} onPress={onPress}>
-            <CustomIcon name="book-outline" size={30} color={newColors.fondo_principal} />
-        </Pressable>
+        <>
+            <Pressable style={styles.button} onPress={openModal}>
+                <CustomIcon name="book-outline" size={30} color={newColors.fondo_principal} />
+            </Pressable>
+
+            <Modalize
+                ref={modalRef}
+                threshold={600}
+                adjustToContentHeight
+                handlePosition="outside"
+                modalStyle={styles.modalContent}
+            >
+                {isOpen && (
+                    <View>
+                        <Text style={globalStyles.titleModal}>Registrar evento</Text>
+                        <CustomSelect 
+                            label="Elige una opción"
+                            value={selectedValue}
+                            options={options.map(option => option.label)} 
+                            onValueChange={(label) => {
+                                const selectedOption = options.find(option => option.label === label);
+                                setSelectedValue(selectedOption ? selectedOption.value : "");
+                            }}
+                        />
+                        <CustomInput 
+                            label="Comentario"
+                            placeholder="Escribe los detalles"
+                            value={comentario}
+                            onChangeText={(value) => setComentario(value)}
+                        />
+                        <View style={{alignItems: 'center'}}>
+                          <CustomButton text="Guardar" onPress={handleSave} />
+                          <CustomButton text="Cancelar" onPress={closeModal} red />
+                        </View>
+                    </View>
+                )}
+            </Modalize>
+        </>
     )
 }
 
 const dynamicStyles = () =>
-  StyleSheet.create({
-    button: {
-      backgroundColor: newColors.verde,
-      position: 'absolute',
-      zIndex: 10,
-      bottom: 80,
-      right: 20,
-      borderRadius: 50,
-      padding: 10,
-      height: 50,
-      width: 50,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
+    StyleSheet.create({
+        button: {
+            backgroundColor: newColors.verde,
+            position: 'absolute',
+            zIndex: 10,
+            bottom: 80,
+            right: 20,
+            borderRadius: 50,
+            padding: 10,
+            height: 50,
+            width: 50,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        modalContent: {
+            padding: 20,
+            backgroundColor: newColors.fondo_principal,
+        },
+    });
 
-export default ButtonAddRegister
+export default ButtonAddRegister;
