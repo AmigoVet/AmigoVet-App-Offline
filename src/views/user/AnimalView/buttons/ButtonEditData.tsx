2344bf4ec9ea00434f721c61df8bc06cd757f45d
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-11 04:20:21 -0500

    fix: Separacion de estilos

--- src/views/user/AnimalView/buttons/ButtonEditData.tsx
+++ src/views/user/AnimalView/buttons/ButtonEditData.tsx
@@ -7,6 +7,7 @@ import { Modalize } from 'react-native-modalize';
 import { pickImageFromGallery, takePhoto } from '../../../../utils/imagePickerUtils';
 import Separator from '../../../../components/global/Separator';
 import { updateAnimal } from '../../../../lib/db/animals/updateDataAnimal';
+import { EditDataEstyles } from './styles/EditDataStyles';
 
 interface ButtonEditDataProps {
     id: string;
@@ -16,7 +17,7 @@ interface ButtonEditDataProps {
 
 const ButtonEditData = ({id,  onPress, animal }: ButtonEditDataProps) => {
     const modalRef = useRef<Modalize>(null);
-    
+    const styles = EditDataEstyles;
     const [selectedValue, setSelectedValue] = useState<string>("");
     const [valueLabel, setValueLabel] = useState<string>("");
     const [propositoValue, setPropositoValue] = useState<string>("");
@@ -202,63 +203,6 @@ const ButtonEditData = ({id,  onPress, animal }: ButtonEditDataProps) => {
     );
 }
 
-const styles = StyleSheet.create({
-    button: {
-        backgroundColor: newColors.verde,
-        position: 'absolute',
-        zIndex: 10,
-        bottom: 140,
-        right: 20,
-        borderRadius: 50,
-        padding: 10,
-        height: 50,
-        width: 50,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modal: {
-        backgroundColor: newColors.fondo_principal,
-        padding: 20,
-        zIndex: 10,
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 15,
-    },
-    imageContainer: {
-        alignItems: "center",
-        marginBottom: 20,
-        borderColor: newColors.verde,
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingHorizontal: 5,
-        paddingVertical: 15,
-    },
-    image: {
-        height: 250,
-        width: '100%',
-    },
-    imageButtonContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-    },
-    imageButton: {
-        marginHorizontal: 20,
-        padding: 10,
-        borderRadius: 5,
-    },
-    noImageText: {
-        fontSize: 16,
-        color: newColors.fondo_principal,
-        marginBottom: 10,
-    },
-    modalActions: {
-        width: '100%',
-        alignItems: 'center',
-    },
-});
+
 
 export default ButtonEditData;
