d3aa3e93e51f004f142b601319be377855804277
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-20 14:41:55 -0500

    style: Contenedores de info

--- src/views/user/AnimalView/sections/ExtraData.tsx
+++ src/views/user/AnimalView/sections/ExtraData.tsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { newColors } from '../../../../assets/styles/colors'
+import { constants } from '../../../../assets/styles/constants'
 
 const ExtraData = () => {
   return (
-    <View>
-      <Text>ExtraData</Text>
+    <View style={styles.container}>
+      <View style={styles.containerContainer}>
+        <Text style={styles.title}>Descripcion</Text>
+        <Text style={styles.info}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non fuga facere sit! Iusto eligendi, esse aliquam voluptatem, itaque ab quos at molestiae officiis corporis dolores obcaecati deleniti distinctio. Ex, eaque.</Text>
+      </View>
+      <View style={styles.containerContainer}>
+        <Text  style={styles.title}>Ubicacion</Text>
+        <Text style={styles.info}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non fuga facere sit! Iusto eligendi, esse aliquam voluptatem, itaque ab quos at molestiae officiis corporis dolores obcaecati deleniti distinctio. Ex, eaque.</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingVertical: 10,
+    gap: 10,
+  },
+  containerContainer: {
+    backgroundColor: newColors.verde,
+    width: '90%',
+    borderRadius: constants.borderRadius,
+    padding: 10,
+    paddingHorizontal: 15,
+    gap: 10,
+  },
+  title:{
+    fontWeight: 'bold',
+    color: newColors.fondo_principal,
+  },
+  info: {
+    color: newColors.principal,
+  }
+})
 
 export default ExtraData
