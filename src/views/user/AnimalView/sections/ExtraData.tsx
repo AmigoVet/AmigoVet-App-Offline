c8cad30a9f1aef3d3256f2d647d19c88e4eb4c68
Juan1mera <juan1mera2barrera@gmail.com>
2025-03-02 13:59:22 -0500

    style: Bordes para texto

--- src/views/user/AnimalView/sections/ExtraData.tsx
+++ src/views/user/AnimalView/sections/ExtraData.tsx
@@ -1,27 +1,27 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { newColors } from '../../../../assets/styles/colors'
-import { constants } from '../../../../assets/styles/constants'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { newColors } from '../../../../assets/styles/colors';
+import { constants } from '../../../../assets/styles/constants';
 
 interface ExtraDataProps {
   description: string;
   ubicacion: string;
 }
 
-const ExtraData = ({description, ubicacion}: ExtraDataProps) => {
+const ExtraData = ({ description, ubicacion }: ExtraDataProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.containerContainer}>
-        <Text style={styles.title}>Descripcion</Text>
+        <Text style={styles.title}>Descripción</Text>
         <Text style={styles.info}>{description}</Text>
       </View>
       <View style={styles.containerContainer}>
-        <Text  style={styles.title}>Ubicacion</Text>
+        <Text style={styles.title}>Ubicación</Text>
         <Text style={styles.info}>{ubicacion}</Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -39,13 +39,21 @@ const styles = StyleSheet.create({
     gap: 10,
     borderWidth: constants.borderWidth,
   },
-  title:{
+  title: {
     fontWeight: 'bold',
     color: newColors.fondo_secundario,
+    borderBottomWidth: 1,
+    borderColor: newColors.fondo_secundario,
+    borderRadius: constants.borderRadius,
+    alignSelf: 'flex-start',
   },
   info: {
     color: newColors.fondo_secundario,
-  }
-})
+    borderBottomWidth: 1,
+    borderColor: newColors.fondo_secundario,
+    borderRadius: constants.borderRadius,
+    alignSelf: 'flex-start',
+  },
+});
 
-export default ExtraData
+export default ExtraData;
