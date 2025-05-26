63ed55c0ddbf130e90ed152653590c612abedee9
Juan Mera <juan1mera2barrera@gmail.com>
2025-05-26 12:26:46 -0500

    chore: start

--- src/lib/store/authStore.ts
+++ src/lib/store/authStore.ts
@@ -17,6 +17,7 @@ interface AuthState {
 const API_BASE_URL = 'https://amigovet-monolitic.zeabur.app/auth';
 
 export const useAuthStore = create<AuthState>((set) => ({
+  // Inicio con supabase
   user: null,
   token: null,
   refreshToken: null,
