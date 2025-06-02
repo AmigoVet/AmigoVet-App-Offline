5c39d03fda009dbce53e54293572a5f136c388d0
Juan Mera <juan1mera2barrera@gmail.com>
2025-06-02 08:46:23 -0500

    chore: Cambio de nombre para coindicir con la PlayStore

--- android/app/src/main/java/com/amigovet/MainActivity.kt
+++ android/app/src/main/java/com/amigovet/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.amigovet
+package com.AmigoVet
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/amigovet/MainApplication.kt
+++ android/app/src/main/java/com/amigovet/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.amigovet
+package com.AmigoVet
 
 import android.app.Application
 import com.facebook.react.PackageList
